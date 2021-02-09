4a557223a76eccb17c83c432f0a55facd62726a0
yanzhenhe <yanzhenhe@ZBMac-C02DN5Q1M.local>
2021-02-09 13:27:21 +0800

    git learn

---
+++ python/main.py
@@ -0,0 +1,13 @@
+
+
+
+def print_hi(name):
+    a = 10
+    a += 5
+    print(a)  # Press ⌘F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
